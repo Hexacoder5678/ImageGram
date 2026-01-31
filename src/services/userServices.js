6cf1d56b7adfc671b291925114ea889485b7907e
Archit Kumar Savita <skumar69688@gmail.com>
2026-01-31 15:17:00 +0530

    test and debug the registration route with pstman and implemented the authencation using jwt

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,5 +1,9 @@
-import { createUser } from "../repositories/userRepository.js";
+import { createUser, findUserByEmail } from "../repositories/userRepository.js";
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+import user from "../schema/user.js";
+import { generateJwtToken } from "../utils/jwt.js";
+
 
 export const signupUserService=async (user)=>{
     try{
@@ -15,3 +19,28 @@ export const signupUserService=async (user)=>{
         throw error;
     }
 }
+
+export const signinUserService=async (userDetails)=>{
+    try{
+        //1.check if there is a valid registered user with the email
+        const user=await findUserByEmail(userDetails.email);
+        if(!user){
+            throw{
+            status:404,
+            message:"User not found"
+        }
+    }
+    //2.compare the password
+    const isPasswordValid=bcrypt.compareSync(userDetails.password,user.password);
+        if(!isPasswordValid){
+            throw{
+                status:401,
+                message:"Invalid password"
+            }
+        }
+        const token=generateJwtToken({email:user.email,_id:user._id,username:user.username});
+        return token;
+    }catch(error){
+            throw error;
+        }
+    }
